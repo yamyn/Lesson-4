460bffd3dbca8bbd1f79a463194f41bdfed4659f
MYN <ricky.month@gmail.com>
2017-12-11 01:23:09 +0200

    Виконав завдання 3, 4

--- src/fv.java
+++ src/fv.java
@@ -3,33 +3,28 @@ import java.util.Scanner;
 public class fv {
     static Scanner scanner = new Scanner(System.in);
     //.......................................................................................//
-    int x = scanner.nextInt();int i = x; // Сканер х
     static void arrayvid() {  // 1. Метод, що рахує числа до значення х
+        System.out.println("Введіть число. на якому слід закінчити відлік: ");
         int x = scanner.nextInt();int i = x; // Сканер х
+        System.out.println();
         int a = 0;
         while (a < x){
             a++;
             System.out.println(a + "\n");
         }
     }
-    /*Задание 2
-    Написать функцию drawRectangle которая рисует в консоли прямоугольник из символов '+'
-    Аргументы функции: ширина прямоугольника в символах, высота прямоугольника в символах
-
-    Например 3 на 2
-    Вывод программы:
-            + + +
-            + + +
-*/
-    static void widthM (int xwindth){
+    //......................................................................................//
+    static void widthM (int xwindth){// метод, що виводить задану ширину квадрата
         int pluswidth = 0;
         while (pluswidth < xwindth) {
             pluswidth++;
             System.out.print("+  ");
         }
     }
-    static void drawRectangle () {
+    static void drawRectangle () {// Метод, що виводить прямокутник в консоль
+        System.out.println("Будь ласка, зазначте ширину прямокутника: ");
         int width = scanner.nextInt();
+        System.out.println("Будь ласка, зазначте довжину прямокутника:");
         int length = scanner.nextInt();
         int pluswidth = 0;
         int pluslength = 0;
@@ -39,10 +34,54 @@ public class fv {
             System.out.println();
         }
     }
+    static void drawRectangle (int z){// Метод, що виводить квадрат в консоль
+        System.out.println("Будь ласка, зазначте довжину сторони квадрата: ");
+        int width = scanner.nextInt();
+        int length = 0;
+        while (length < width) {
+            length++;
+            widthM(width);
+            System.out.println();
+        }
+    }
+    //.....................................................................................//
+    static void getMax (){
+        System.out.println("Введіть значення першого аргумента:");
+    int a = scanner.nextInt();
+        System.out.println("Введіть значення другого аргумента: ");
+    int b = scanner.nextInt();
+    if (a > b){
+        System.out.println(a + " - більше");
+        } else if (a < b){
+             System.out.println(b + " - більше");
+              } else if (a == b){
+                  System.out.println("Елементи рівні!!!");
+                     }
+    }
+    static void getMax (float ax, float bx){
+        System.out.println("Введіть значення першого аргумента:");
+        float a = scanner.nextFloat();
+        System.out.println("Введіть значення другого аргумента: ");
+        float b = scanner.nextFloat();
+            if (a > b){
+              System.out.println(a + " - більше");
+            } else if (a < b){
+                System.out.println(b + " - більше");
+                 } else if (a == b){
+                    System.out.println("Елементи рівні!!!");
+                    }
+    }
 
     public static void main(String[] args) {
-        arrayvid(); // Виклик методу задачі 1
-        drawRectangle();
+        System.out.println("Вітаю!!!");
+        //arrayvid(); // Виклик методу задачі 1
+        //drawRectangle();// Виклик методу задачі 2
+        /*Я понимаю, что такая запись кода может быть не совсем верна, но если писать сканер в самом методе то код
+        более читабельный, чем писать в главном методе все сканеры, а потом передавать, но так как в домашнем задании
+        нужно было использовать перезагрузку, я не создал новый метод, а вставил аргумент который не используеться*/
+        //drawRectangle(0);//Виклик задачі3
+        //getMax();
+        getMax(0.0f ,0.0f);
 
 
     }
